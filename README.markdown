68da184b28bb7206ce7198fb076298b2a17a27f4
Matthew Seiler <theflyinghyrax@gmail.com>
2014-12-23 18:40:44 -0500

    Tweak readme and setup file

--- README.markdown
+++ README.markdown
@@ -1,3 +1,3 @@
-* Simple simulation of that triangular peg jumping game from Cracker Barrel *
+_Simple simulation of that triangular peg jumping game from Cracker Barrel_
 
-
+( Mostly, this just exists to learn how to use pip/virtualenv/pytest/etc. )
--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name='peg_game_sim',
     version='0.0b1',
     description='Solve the CB peg game',
+    url='https://github.com/mr-seiler/peggy',
     license='MIT',
     packages=['peggy'],
     extras_require={
-        'dev': ['pytest'],
+        'dev': ['pytest','coverage'],
     },
     entry_points={
         'console_scripts': [
